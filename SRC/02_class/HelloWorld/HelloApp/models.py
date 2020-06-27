3558e76661a0d92344c82f7387033013a6d01521
Animesh Bhadra <animeshb@archerimagine.com>
2020-06-27 18:38:21 +0530

    👌 IMPROVE: Hello Customer admin panel

--- SRC/02_class/HelloWorld/HelloApp/models.py
+++ SRC/02_class/HelloWorld/HelloApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class HelloCustomer(models.Model):
+    full_name = models.CharField(max_length=60)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.TextField(blank=True)
+    pincode = models.IntegerField()
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.full_name, self.phone, self.email
