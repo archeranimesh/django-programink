924c3c384db6e6a90926cd6949e6ca6dd5db1976
Animesh Bhadra <animeshb@archerimagine.com>
2020-06-28 15:47:51 +0530

    🐛 FIX: Fixed the Date of birth.

--- SRC/02_class/HelloWorld/HelloApp/models.py
+++ SRC/02_class/HelloWorld/HelloApp/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class HelloCustomer(models.Model):
     full_name = models.CharField(max_length=60)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15, unique=True)
     email = models.EmailField()
     address = models.TextField(blank=True)
-    pincode = models.IntegerField()
-    dob = models.DateField()
+    pincode = models.IntegerField(default=0)
+    dob = models.DateField(verbose_name="Date of Birth")
 
-    def __str__(self):
-        return self.full_name + " : " + self.phone + " : " + self.email
+    def __str__(self):  # string representation of object
+        return self.full_name
