c8d834bc2144aaf1fa2173cc84e57753bd8397a7
Animesh Bhadra <animeshb@archerimagine.com>
2020-06-28 16:05:09 +0530

    👌 IMPROVE: Added the gender choice field.

--- SRC/02_class/HelloWorld/HelloApp/models.py
+++ SRC/02_class/HelloWorld/HelloApp/models.py
@@ -8,6 +8,11 @@ class HelloCustomer(models.Model):
     address = models.TextField(blank=True)
     pincode = models.IntegerField(default=0)
     dob = models.DateField(verbose_name="Date of Birth")
+    # The below drop down menu option was added,
+    # once added we have to run migrate and makemigration.
+    gender = models.CharField(
+        max_length=30, choices=(("Male", "Male"), ("Female", "Female")), default="Male"
+    )
 
     def __str__(self):  # string representation of object
         return self.full_name
