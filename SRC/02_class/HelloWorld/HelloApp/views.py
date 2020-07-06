eafd9525c7d5a1c0122ab2e930942431f6d137e7
Animesh Bhadra <animeshb@archerimagine.com>
2020-07-06 23:15:50 +0530

    👌 IMPROVE: QuerySet question.

--- SRC/02_class/HelloWorld/HelloApp/views.py
+++ SRC/02_class/HelloWorld/HelloApp/views.py
@@ -20,3 +20,13 @@ def addcandidate(request):
 def viewcandidates(request):
     candidates = HelloCustomer.objects.all()
     return render(request, "viewcandidates.html", {"candidates": candidates})
+    # Below code is for query sets.
+    # q = HelloCustomer.objects.values("full_name", "phone", "email", "dob")
+    # And , OR
+    # q = HelloCustomer.objects.filter(full_name__contains="Animesh")
+    # p = HelloCustomer.objects.filter(full_name__contains="Aru")
+    # Order By
+    # q = HelloCustomer.objects.all().order_by("full_name")
+    # order-by reverse
+    # q = HelloCustomer.objects.all().order_by("-full_name")
+    # return render(request, "viewcandidates.html", {"candidates": q})
